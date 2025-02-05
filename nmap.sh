f7b0be5de231f3353f09fbe0e914e473dff30925
trop3n <jasondkimm@proton.me>
2025-02-04 21:40:29 -0600

    script update

--- nmap.sh
+++ nmap.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-nmap $1
-/path/to/dirsearch.py -u $1 -e php
+#!/bin/bash 
+echo "Creating directory $1_recon." 1 
+mkdir $1_recon 2 
+nmap $1 > $1_recon/nmap 3 
+echo "The results of nmap scan are stored in $1_recon/nmap." /PATH/TO/dirsearch.py -u $1 -e php 4 --simple-report=$1_recon/dirsearch echo "The results of dirsearch scan are stored in $1_recon/dirsearch."
