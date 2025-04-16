f03e55100428331a5733c6c255102a83d15b656b
trop3n <jasondkimm@proton.me>
2025-04-15 22:56:01 -0500

    php code that prints results of SQL query

--- intro_to_sqli/sql_conn_php.php
+++ intro_to_sqli/sql_conn_php.php
@@ -2,4 +2,8 @@
 $conn = new mysqli("localhost", "root", "password", "users");
 $query = "select * from logins";
 $result = $conn->query($query);
+
+while($row = $result->fetch_assoc() ){
+    echo $row["name"]."<br>";
+}
 ?>
