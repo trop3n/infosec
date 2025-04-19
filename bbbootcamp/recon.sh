3da7a0157cce399b3ed1ea5b9d6cf074b4c64e71
trop3n <jasondkimm@proton.me>
2025-04-18 23:36:57 -0500

    added report_domain() function

--- bbbootcamp/recon.sh
+++ bbbootcamp/recon.sh
@@ -33,42 +33,22 @@ scan_domain(){
         ;;
     esac
 }
-for i in "${@:OPTIND:$#}" # 1
-do
-  DOMAIN=$i
+report_domain(){
+  DOMAIN=$1
   DIRECTORY=${DOMAIN}_recon
-  echo "Creating directory $DIRECTORY."
-  mkdir $DIRECTORY
-
-  case $MODE in
-    nmap-only)
-      nmap_scan
-      ;;
-    dirsearch-only)
-      dirsearch_scan
-      ;;
-    crt-only)
-      crt_scan
-      ;;
-    *)
-      nmap_scan
-      dirsearch_scan
-      crt_scan
-      ;;
-esac
-echo "Generating recon report for $DOMAIN..."
-TODAY=$(date)
-echo "This scan was created on $TODAY" > $DIRECTORY/report
-  if [ -f $DIRECTORY/nmap ];then # 2
-  echo "Results for Nmap:" >> $DIRECTORY/report
-  grep -E "^\s*\S+\s+\S+\s+\S+\s*$" $DIRECTORY/nmap >> $DIRECTORY/report
-fi
-  if [ -f $DIRECTORY/dirsearch ];then # 3
-  echo "Results for Dirsearch:" >> $DIRECTORY/report
-  cat $DIRECTORY/dirsearch >> $DIRECTORY/report
-fi
-  if [ -f $DIRECTORY/crt ];then # 4
-  echo "Results for crt.sh:" >> $DIRECTORY/report
-  jq -r ".[] | .name_value" $DIRECTORY/crt >> $DIRECTORY/report
-fi
-done # 4
+  echo "Generating recon report for $DOMAIN..."
+  TODAY=$(date)
+  echo "This scan was created on $TODAY" > $DIRECTORY/report
+  if [ -f $DIRECTORY/nmap ];then
+    echo "Results for Nmap:" >> $DIRECTORY/report
+    grep -E "^\s*\S+\s+\S+\s+\S+\s*$" $DIRECTORY/nmap >> $DIRECTORY/report
+  fi
+  if [ -f $DIRECTORY/dirsearch ];then
+    echo "Results for Dirsearch:" >> $DIRECTORY/report
+    cat $DIRECTORY/dirsearch >> $DIRECTORY/report
+  fi
+  if [ -f $DIRECTORY/crt ];then
+    echo "Results for crt.sh:" >> $DIRECTORY/report
+    jq -r ".[] | .name_value" $DIRECTORY/crt >> $DIRECTORY/report
+  fi
+}
