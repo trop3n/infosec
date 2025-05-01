43fd39a8398d5ec763e4e9033f1920ceb08fc16a
trop3n <jasondkimm@proton.me>
2025-04-30 19:41:17 -0500

    basic hello world flask app for testing

--- race-conditions/flask_app/flask_app.py
+++ race-conditions/flask_app/flask_app.py
@@ -1,1 +1,13 @@
 from flask import Flask
+
+# create an instance of the flask class representing the application
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<html><head><title>Greeting</title></head><body><h1>Hello, World!</h1></body></html>'
+
+# this checks if the program is being run as a module or the main program
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
