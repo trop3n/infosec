365370eca6f9534371d669bfedff42b8f86bed52
trop3n <jasondkimm@proton.me>
2025-05-02 18:07:19 -0500

    added and started threads

--- race-conditions/race_to_100.py
+++ race-conditions/race_to_100.py
@@ -1,2 +1,14 @@
 import threading
 import time
+
+def increase_by_10():
+    for i in range(1, 10):
+        print(f"Thread {threading.current_thread().name}: {i}0% complete")
+
+# create two threads
+thread1 = threading.Thread(target=increase_by_10, name="Thread-1")
+thread2 = threading.Thread(target=increase_by_10, name="Thread-2")
+
+#start threads
+thread1.start()
+thread2.start()
