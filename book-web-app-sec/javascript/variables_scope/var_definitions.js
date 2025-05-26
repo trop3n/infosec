0754a7a854cf9be59afff10bff9a05aa5f027f44
trop3n <jasondkimm@proton.me>
2025-05-25 23:29:43 -0500

    different ways to define a variable in JS

--- book-web-app-sec/javascript/variables_scope/var_definitions.js
+++ book-web-app-sec/javascript/variables_scope/var_definitions.js
@@ -9,3 +9,12 @@ let age = 25;
 
 // block scoped, without reassignment
 const age = 25;
+
+// define global integer
+age = 25;
+
+// direct call (returns 25)
+console.log(age);
+
+// call via pointer on window (returns 25)
+console.log(window.age);
