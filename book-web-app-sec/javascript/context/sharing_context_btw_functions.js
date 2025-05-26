449321b854ff4bcf450915cc8a87b55076853f5a
trop3n <jasondkimm@proton.me>
2025-05-25 23:55:51 -0500

    added explanatory comments

--- book-web-app-sec/javascript/context/sharing_context_btw_functions.js
+++ book-web-app-sec/javascript/context/sharing_context_btw_functions.js
@@ -7,3 +7,10 @@ const boundAge = getAge.call(ageData, 'joe');
 
 // call getAge() with ageData context and param joe
 const boundAge  = getAge.apply(ageData, ['joe']);
+
+/*
+ * bind, call and apply allow developers to move context from one function to another.
+ *
+ * The only difference between call, and apply is that call takes a list of arguments,
+ * and apply takes and array of arguments.
+ */
