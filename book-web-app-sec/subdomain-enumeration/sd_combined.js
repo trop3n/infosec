7a259b1b4f0ab6b8a3dcd40174f35562f7fd5de7
trop3n <jasondkimm@proton.me>
2025-05-31 08:29:09 -0500

    added dnscan functionality

--- book-web-app-sec/subdomain-enumeration/sd_combined.js
+++ book-web-app-sec/subdomain-enumeration/sd_combined.js
@@ -1,4 +1,38 @@
 const dns = require('dns');
+const csv = require('csv')
+const fs = require('fs');
+const { resolve } = require('path');
+
+const promises = [];
+
+/*
+ * Begin streaming the subdomain data from disk (versus
+ * pulling it all into memory at once, in case it is a large file).
+ * 
+ * On each line, call `dns.resolve` to query the subdomain and
+ * check if it exists. Store these promises in the `promises` array.
+ * 
+ * When all lines have been read, and all promises have been resolved,
+ * then log the subdomains found to the console.
+ * 
+ * Performance upgrade: if the subdomains list is exceptionally large,
+ * then a second file should be opened and the results should be
+ * streamed to that file whenever a promise resolves.
+ */
+
+fs.createReadStream('subdomains.10000.txt')
+  .pipe(csv())
+  .on('data', (subdomain) => {
+    promises.push(new Promise((resolve, reject) => {
+      dns.resolve(`${subdomain}.mega-bank.com`, function (err, ip) {
+        return resolve({ subdomain, ip: ip });
+      });
+    }));
+  })
+  .on('end', () => {
+
+    // after all of the DNS queries have completed, log the results
+  })
 
 /*
  * A simple function for brute forcing a list of subdomains
