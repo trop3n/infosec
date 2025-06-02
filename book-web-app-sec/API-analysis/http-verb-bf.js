a615279f3295f6742a214d7173787246b4255779
trop3n <jasondkimm@proton.me>
2025-06-01 21:25:30 -0500

    started HTTP verb brute-force script

---
+++ book-web-app-sec/API-analysis/http-verb-bf.js
@@ -0,0 +1,11 @@
+/*
+ * If the request is successful, resolve the promise and
+ * include the status code in the result.
+ */
+
+const discoverHTTPVerbs = function(url) {
+    const verbs = ['POST', 'GET', 'PUT', 'PATCH', 'DELETE'];
+    const promises = [];
+
+    
+}
