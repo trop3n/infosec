8f846df3450528422effd0a0da35c17541fc9a0d
trop3n <jasondkimm@proton.me>
2025-07-05 12:50:01 -0500

    beginning XSS module of the book

--- book-web-app-sec/part_II/XSS_intro.js
+++ book-web-app-sec/part_II/XSS_intro.js
@@ -6,3 +6,10 @@ const comment = 'my <strong>comment</strong>';
 const div = document.createElement('div');
 div.innerHTML = comment;
 
+/*
+ * Append the div to the DOM, with it the innerHTML DOM from the comment.
+ * Because the comment is interpreted as DOM, it will be parsed
+ * and translated into DOM elemnts upon load.
+ */
+const wrapper = document.querySelector('#commentArea');
+wrapper.appendChild(div);
