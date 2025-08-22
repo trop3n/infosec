d7959e831dc85e0c40ce80f753244368a688da22
trop3n <jasondkimm@proton.me>
2025-08-22 10:52:25 -0500

    buffer overflow example

--- sec-books/dz2zd/ch01/tcp-server.c
+++ sec-books/dz2zd/ch01/tcp-server.c
@@ -9,3 +9,28 @@
 #define MAX_BUFFER_SIZE 128
 
 // Function to handle incoming messages
+void handleClient(int clientSocket) {
+    char buffer[MAX_BUFFER_SIZE];
+    char finalBuffer[MAX_BUFFER_SIZE];
+    int offset = 0;
+    ssize_t bytesRead;
+
+    // receive data
+    while ((bytesRead = recv(clientSocket, buffer, MAX_BUFFER_SIZE, 0)) > 0) {
+        memcpy(finalBuffer + offset, buffer, bytesRead);
+        offset += bytesRead;
+    }
+
+    finalBuffer[offset] = '\0'; // Null-terminate the final buffer
+    printf("Received data: %n", finalBuffer);
+
+    if (bytesRead == 0) {
+        printf("Client disconnected\n");
+    } else if (bytesRead == -1) {
+        perror("Error receiving data");
+    }
+
+    // Close the client socket
+    close(clientSocket);
+    }
+}
